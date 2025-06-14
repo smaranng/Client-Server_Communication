5167ef6ec13ce6dcc90fc73c59fc06a8dcad6bcf
Smaran <smaran.ng@gmail.com>
2025-06-14 14:16:27 +0530

    Updated commit

--- Server/2.py
+++
@@ -1,9 +0,0 @@
-def calculate_price(n):
-    digits = str(n)
-    product = 1
-    for digit in digits:
-        product *= int(digit)
-    return product
-n = int(input("Enter the value of N: "))
-price = calculate_price(n)
-print("Output:", price)
